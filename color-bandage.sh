39d273901a3a13119e80f53613f0969d0371db69
rec3141 <rec3141@gmail.com>
2018-10-06 14:42:39 -0800

    Update color-bandage.sh

--- color-bandage.sh
+++ color-bandage.sh
@@ -12,19 +12,16 @@ for file in $GFA; do
 	echo "start $file"
 
 	f1=`basename $file .gfa`
-	mkdir $f1
-	cd $f1
-	ln -s ./../$file $file
 	grep '^S'  $f1.gfa | cut -f2,3 | sed 's/^\([0-9]\)/>\1/' | tr $'\t' $'\n' > $f1.assembly.fasta
 
-    	#run sendsketch
-    	sendsketch.sh in=$f1.assembly.fasta address=nt mode=sequence records=1 out=$f1.sketch.tsv ow=t
+   	#run sendsketch
+   	sendsketch.sh in=$f1.assembly.fasta address=nt mode=sequence records=1 out=$f1.sketch.tsv ow=t
 
 	# match orfs to nodes
 	awk -v OFS=$'\t' 'BEGIN{feature=0}{if($0 ~ /^Query/){feature=$2};if($0 ~ /^[0-9]/){print feature,$12}}' $f1.sketch.tsv | sort -k2,2 | tr $'\t' ',' > $f1.nodes.csv
 
 	#get orf colors
-    	cut -f2 -d',' $f1.nodes.csv | uniq > $f1.names.tsv
+	cut -f2 -d',' $f1.nodes.csv | uniq > $f1.names.tsv
 	while read line; do echo "#"`md5 <(echo -e $line) | cut -f4 -d' '| cut -b1-6`;  done < $f1.names.tsv > $f1.colors.tsv
 	paste -d',' $f1.colors.tsv $f1.names.tsv > $f1.taxcolors.csv
 	
@@ -33,6 +30,6 @@ for file in $GFA; do
     	join -1 2 -2 2 -t',' $f1.nodes.csv $f1.taxcolors.csv | awk -v OFS=',' -v FS=',' '{print $2,$1,$3}' >> $f1.bandage.csv
     
 	echo "done $file"
-	cd -
-done;
 
+    rm $f1.assembly.fasta $f1.sketch.tsv $f1.names.tsv $f1.colors.tsv $f1.taxcolors.csv $f1.nodes.csv
+done;
